0957c8652862b53186b8ae2f1f7444f380546d6f
William <will@penguin.com>
2024-05-08 16:48:27 -0400

    pushing

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,4 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        if i == 9 and j == 9:
-            print("{:d}{:d}".format(i, j))
-        else:
-            print("{:d}{:d}".format(i, j), end=", ")
+        print("{:d}{:d}".format(i, j), end=", " if i != 9 or j != 8 else "\n")
