1af4b0b84678755fa57fea498ae931a016bff45c
William <will@penguin.com>
2024-05-08 22:40:59 -0400

    fixing more stuff

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+def uppercase(s):
+    for char in s:
+        if 'a' <= char <= 'z':
+            print(chr(ord(char) - 32), end='')
+        else:
+            print(char, end='')
+    print()
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
