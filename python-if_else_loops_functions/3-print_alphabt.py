bd427d188905c84ce784871fae10fb57b85f13bc
William <will@penguin.com>
2024-05-08 22:34:44 -0400

    fixing more excercise

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(ord('a'), ord('z') + 1):
-    if chr(i) not in ['q', 'e']:
-        print(chr(i), end='')
+for bet in range(97, 123):
+    if chr(bet) not in ['q', 'e']:
+        print("{}".format(chr(bet)), end='')
