32791a58f7d4df84e32780ccce054788036558fd
William <will@penguin.com>
2024-05-10 01:32:48 -0400

    more fixes

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    import calculator_1
-
-    result_add = calculator_1.add(a, b)
-    result_subtract = calculator_1.subtract(a, b)
-    result_multiply = calculator_1.multiply(a, b)
-    result_divide = calculator_1.divide(a, b)
-
+    import calculator_1 as calc
+    
+    result_add = calc.add(a, b)
+    result_subtract = calc.subtract(a, b)
+    result_multiply = calc.multiply(a, b)
+    result_divide = calc.divide(a, b)
+    
     print("Addition: {} + {} = {}, Subtraction: {} - {} = {}, Multiplication: {} * {} = {}, Division: {} / {} = {}".format(a, b, result_add, a, b, result_subtract, a, b, result_multiply, a, b, result_divide))
