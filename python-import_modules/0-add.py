2bb8276c8d7f57e6c217412e8df08f9f9539ff5a
William <will@penguin.com>
2024-05-10 13:56:47 -0400

    pushing

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
+
 if __name__ == "__main__":
-    a = 1
-    b = 2
-
-    import_module = __import__('add_0')
-    add = import_module.add
-
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    a = 10
-    b = 5
+from calculator_1 import add, sub, mul, div
 
-    import calculator_1 as calc
-    
-    result_add = calc.add(a, b)
-    result_subtract = calc.subtract(a, b)
-    result_multiply = calc.multiply(a, b)
-    result_divide = calc.divide(a, b)
-    
-    print("Addition: {} + {} = {}, Subtraction: {} - {} = {}, Multiplication: {} * {} = {}, Division: {} / {} = {}".format(a, b, result_add, a, b, result_subtract, a, b, result_multiply, a, b, result_divide))
+a, b = 10, 5
+
+if __name__ == "__main__":
+    results = {
+        "Addition": add(a, b),
+        "Subtraction": sub(a, b),
+        "Multiplication": mul(a, b),
+        "Division": div(a, b)
+    }
+
+    for operation, result in results.items():
+        print(f"{a} {'+' if operation == 'Addition' else '-' if operation == 'Subtraction' else '*' if operation == 'Multiplication' else '/'} {b} = {result}")
