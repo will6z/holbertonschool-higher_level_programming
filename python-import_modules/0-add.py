923f2ca0861693f95bee9ca1c1b408f0455b855a
William <will@penguin.com>
2024-05-10 01:27:21 -0400

    fixing]

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -3,8 +3,7 @@ if __name__ == "__main__":
     a = 1
     b = 2
 
-    add_0_module = __import__('add_0')
-    add = add_0_module.add
+    add = __import__('add_0').add
 
     result = add(a, b)
     print("{} + {} = {}".format(a, b, result))
