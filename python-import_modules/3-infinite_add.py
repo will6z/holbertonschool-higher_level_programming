808a54d04c2de84326b5bdfdfdab2f229ba037c2
William <will@penguin.com>
2024-05-10 14:12:52 -0400

    pushing (runs good, should be no errors)

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,10 +2,7 @@
 import sys
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
+    arguments = sys.argv[1:]
+    result = sum(int(arg) for arg in arguments)
+    print(result)
 
-    if args:
-        total = sum(int(arg) for arg in args)
-        print(total)
-    else:
-        print(0)
