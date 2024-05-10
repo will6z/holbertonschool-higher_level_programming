a91c3889e411b82c2ce87dd76e19d8593ec53d83
William <will@penguin.com>
2024-05-10 01:34:18 -0400

    lol

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+if __name__ == "__main__":
+    import sys
+
+    args = sys.argv[1:]
+    num_args = len(args)
+
+    print("Number of argument{}: {}{}".format('' if num_args == 1 else 's', num_args, '' if num_args == 0 else ':'))
+
+    if num_args > 0:
+        for i, arg in enumerate(args, 1):
+            print("{}: {}".format(i, arg))
+    else:
+        print(".")
+
