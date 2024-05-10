86c50d46318e12e6640cf26d51fb72911b65eec7
William <will@penguin.com>
2024-05-10 01:25:15 -0400

    fixing

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-a = 1
-b = 2
+if __name__ == "__main__":
+    a = 1
+    b = 2
 
-add_0_module = __import__('add_0')
-add = add_0_module.add
+    add_0_module = __import__('add_0')
+    add = add_0_module.add
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
