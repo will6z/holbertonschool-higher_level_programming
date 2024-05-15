17a729fe8fda366665bbb0e728eb957c2a1987aa
William <will@penguin.com>
2024-05-15 12:56:00 -0400

    0 fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        for i in range(x):
-            print(my_list[i], end="")
-    except IndexError:
-        pass
-    finally:
-        print()
-    return min(x, len(my_list))
+    sum_total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            sum_total += 1
+        except IndexError:
+            break
+    print()
+    return(sum_total)
