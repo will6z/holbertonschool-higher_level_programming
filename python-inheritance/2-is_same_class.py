75e3da375cbc381a68beedf7c0aaa5bbb2fdb192
William <will@penguin.com>
2024-05-22 17:17:53 -0400

    2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return type(obj) is a_class
+"""
+makin a function that returns true if the obj is the same instance
+as the specified class
+"""
 
+
+def is_same_class(obj, a_class):
+    """
+    returns the type() of obj and compares to a_class
+    Args:
+        obj
+        a_class
+    """
+    return type(obj) is a_class
