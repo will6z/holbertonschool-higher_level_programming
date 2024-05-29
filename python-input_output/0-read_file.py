db962685589b80297b9201c64cfe128114defee6
William <will@penguin.com>
2024-05-28 22:37:01 -0400

    1

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This function writes a string to .txt file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    In this the code that return the number of char
+    """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return f.write(text)
