8c324925a6cd2511aa8585a2570ca6195dfa1ed1
William <will@penguin.com>
2024-05-28 22:46:40 -0400

    3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+"""
+makin a func that returns the JSON representation of an obj
+"""
+
+import json
+
+def to_json_string(my_obj):
+
+    """
+    func returns the JSON representation of an obj
+    """
+    return json.dumps(my_obj)
