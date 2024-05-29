01c76a5e9f454e609510045bd864fda6e4754d4e
William <will@penguin.com>
2024-05-28 23:09:56 -0400

    12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
+    """Returns a list"""
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Start with the first row
+    triangle = [[1]]
 
     for i in range(1, n):
-        row = [1]  # Start each row with a 1
+        prev_row = triangle[-1]
+        new_row = [1]
+
         for j in range(1, i):
-            # Each element is the sum of the two elements above it
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
-        row.append(1)  # End each row with a 1
-        triangle.append(row)
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
 
     return triangle
-
-# Example usage
-n = 5
-print(pascal_triangle(n))
 
