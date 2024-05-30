ab58803ed2c315faef33e847b059a5b9457f8179
William <will@penguin.com>
2024-05-29 23:22:50 -0400

    1 fixed

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,40 +1,35 @@
 #!/usr/bin/python3
-
+"""Serialize and Deserialize using pickle"""
 import pickle
 
 class CustomObject:
     def __init__(self, name, age, is_student):
+        """Initialize attributes"""
         self.name = name
         self.age = age
         self.is_student = is_student
 
     def display(self):
-        """
-        print out the objs attributes in the specified format.
-        """
+        """Display object attributes"""
         print(f"Name: {self.name}")
         print(f"Age: {self.age}")
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
-        """
-        serialize the current instance of the object and save it to the provided filename
-        """
+        """Serialize object to a file"""
         try:
             with open(filename, 'wb') as file:
                 pickle.dump(self, file)
-        except Exception as e:
-            print(f"An error occurred while serializing: {e}")
-    
+        except Exception as error:
+            print(f"Error serializing object: {error}")
+
     @classmethod
     def deserialize(cls, filename):
-        """
-        load and return an instance of CustomObject from the provided filename
-        """
+        """Deserialize object from a file"""
         try:
             with open(filename, 'rb') as file:
-                obj = pickle.load(file)
-                return obj
-        except (FileNotFoundError, pickle.UnpicklingError) as e:
-            print(f"An error occurred while deserializing: {e}")
+                return pickle.load(file)
+        except Exception as error:
+            print(f"Error deserializing object: {error}")
             return None
+
