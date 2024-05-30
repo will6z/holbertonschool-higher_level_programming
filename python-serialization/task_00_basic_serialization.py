3bd1ca616a571fec655b2fe15ad9effe639c376d
William <will@penguin.com>
2024-05-29 23:24:00 -0400

    00 fixed as well

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
-    """ 
-    basic serialization module
-    """
+"""Basic serialization module"""
 import json
 
 def serialize_and_save_to_file(data, filename):
-    """
-    serialize a dictionary and save it to a JSON file
-    """
+    """Serialize dict to JSON file"""
     with open(filename, 'w') as file:
-        json.dump(data, file, indent=4)
+        json.dump(data, file)
 
 def load_and_deserialize(filename):
-    """
-    Load data from a JSON file and deserialize it to a Python dict 
-    """
+    """Deserialize JSON file to dict"""
     with open(filename, 'r') as file:
-        data = json.load(file)
-    return data
+        return json.load(file)
 
