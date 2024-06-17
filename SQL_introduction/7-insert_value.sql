cc662e2265869a6268fd4ed94d0462cfc3326c32
William <will@penguin.com>
2024-06-17 18:16:30 -0400

    7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,5 +1,3 @@
 -- This script inserts a new row into the table first_table in the specified database.
 
-INSERT INTO hbtn_0c_0.first_table (id, name)
-VALUES (89, 'Best School');
-
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
