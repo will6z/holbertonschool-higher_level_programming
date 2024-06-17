cef88b563ddc3d3d6e7d1d2517ef977a26a6e623
William <will@penguin.com>
2024-06-17 18:21:58 -0400

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,13 +1,12 @@
 -- This script creates a table second_table in the specified database and inserts multiple rows.
 
-CREATE TABLE IF NOT EXISTS hbtn_0c_0.second_table (
-    id INT,
-    name VARCHAR(256),
-    score INT
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256),
+  score INT
 );
 
-INSERT INTO hbtn_0c_0.second_table (id, name, score)
-VALUES (1, 'John', 10),
-       (2, 'Alex', 3),
-       (3, 'Bob', 14),
-       (4, 'George', 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
