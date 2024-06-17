d6735ee24d71de2428b5f784bc94679aa79a8e9b
William <will@penguin.com>
2024-06-17 18:15:03 -0400

    5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,10 +1,3 @@
 -- This script creates the table first_table with specified columns and properties.
 
-CREATE TABLE IF NOT EXISTS `first_table` (
-    `id` int NOT NULL AUTO_INCREMENT,
-    `name` varchar(128) DEFAULT NULL,
-    `c` char(1) DEFAULT NULL,
-    `created_at` date DEFAULT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+SHOW CREATE TABLE first_table;
