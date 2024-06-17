2a4d38c4d803e189f6c7c7b05a805c3e283d5973
William <will@penguin.com>
2024-06-17 18:37:45 -0400

    16

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,7 +1,7 @@
 -- This script lists all records from the table second_table.
 
 SELECT score, name
-FROM hbtn_0c_0.second_table
-WHERE name IS NOT NULL AND name != ''
+FROM second_table
+WHERE name IS NOT NULL
 ORDER BY score DESC;
 
