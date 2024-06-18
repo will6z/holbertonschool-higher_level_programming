00886554ac3c56668f1289e90a691889258b4516
William <will@penguin.com>
2024-06-18 18:00:14 -0400

    8

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,6 @@
+-- Select cities with corresponding state names using JOIN
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id;
+
