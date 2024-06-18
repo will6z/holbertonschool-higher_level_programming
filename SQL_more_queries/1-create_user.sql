5a0dab14ce3263539d24f6f4670c556011ffe5fb
William <will@penguin.com>
2024-06-18 17:31:44 -0400

    1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,9 @@
+-- Create user user_0d_1 with the specified password if not exists
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- Flush privileges to ensure the changes take effect
+FLUSH PRIVILEGES;
+
