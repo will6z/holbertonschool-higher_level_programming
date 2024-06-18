bd5b3af9c76bba49991625cc0626658da0bf1910
William <will@penguin.com>
2024-06-18 17:54:07 -0400

    6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,12 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the created database
+USE hbtn_0d_usa;
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT UNIQUE NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
+
