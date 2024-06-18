a9252966034e48a911273b59f904b89c2d11d026
William <will@penguin.com>
2024-06-18 17:37:25 -0400

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,9 +1,5 @@
--- Create user user_0d_1 with the specified password if not exists
+-- Create user user_0d_1 with secure password (replace with your desired password)
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
-
--- Show grants for user_0d_1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-
+-- Grant all privileges on *.* to user_0d_1
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
