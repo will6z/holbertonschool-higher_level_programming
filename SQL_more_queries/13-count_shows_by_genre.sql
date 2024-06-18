adbcb28ef7f42b3b86c1908f8c5d00d73666243a
William <will@penguin.com>
2024-06-18 18:31:12 -0400

    13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,4 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
 FROM tv_show_genres
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
-ORDER BY number_of_shows DESC;
-
+ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
