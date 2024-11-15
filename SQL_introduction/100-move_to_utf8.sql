5322abe87860234e00e14ae93ed8e905522fdabe
William <will@penguin.com>
2024-11-14 22:51:25 -0400

    100

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -7,6 +7,5 @@ ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 -- Convert table first_table to UTF8 (utf8mb4) with utf8mb4_unicode_ci collation
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
--- Convert field name in first_table to UTF8 (utf8mb4) with utf8mb4_unicode_ci collation
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
+-- Convert field name in first_table to use utf8mb4_unicode_ci collation
+ALTER TABLE first_table MODIFY name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
