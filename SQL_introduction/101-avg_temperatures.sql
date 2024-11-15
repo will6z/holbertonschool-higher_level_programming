d9f6e47ce45fe36e2d3d4796fe034ba50e925d70
William <will@penguin.com>
2024-11-14 20:30:05 -0400

    101

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,10 +1,14 @@
+-- Select the city and calculate the average temperature rounded to four decimal places
 SELECT
     city,
     ROUND(AVG(temperature), 4) AS avg_temp
+-- Specify the table from which data is retrieved
 FROM
     temperatures
+-- Group the results by city to calculate the average temperature per city
 GROUP BY
     city
+-- Order the results by average temperature in descending order
 ORDER BY
     avg_temp DESC;
 
